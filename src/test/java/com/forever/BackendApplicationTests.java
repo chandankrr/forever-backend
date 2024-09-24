c003cb3596a7bcfa9f78ab14d4dc328beb2f4342
Chandan Kumar <chandankrr.91@gmail.com>
2024-09-24 14:08:08 +0530

    Initial commit

---
+++ src/test/java/com/forever/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
